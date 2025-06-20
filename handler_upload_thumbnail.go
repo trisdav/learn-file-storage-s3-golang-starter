bbc40a2daf3556ce9b11c286a048c8c48f10d948
Tristan Davis <tridav95@gmail.com>
2025-06-20 15:27:11 -0500

    Ch1l5

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"io"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -32,6 +33,47 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10<<20
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	parsErr := r.ParseMultipartForm(maxMemory)
+	if parsErr != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't parse multipart form", parsErr)
+		return
+	}
+
+	file, header, formErr := r.FormFile("thumbnail")
+	if formErr != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't parse thumbnail", formErr)
+		return
+	}
+	media := header.Header.Get("Content-Type")
+
+	tData, readErr := io.ReadAll(file)
+	if readErr != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't parse thumbnail data", readErr)
+		return
+	}
+
+	vid, getErr := cfg.db.GetVideo(videoID)
+	if getErr != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't retrive video data from the database", getErr)
+		return
+	}
+
+	if vid.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "You are not the author of that video", nil)
+		return	
+	}
+
+	newTn := thumbnail{data:tData,mediaType:media,}
+	videoThumbnails[videoID] = newTn
+	newTnUrl := fmt.Sprintf("http://localhost:8091/api/thumbnails/%s",videoID.String())
+	vid.ThumbnailURL = &newTnUrl
+	updatErr := cfg.db.UpdateVideo(vid)
+	if updatErr != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't update video data in the database", updatErr)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, vid)
 }
